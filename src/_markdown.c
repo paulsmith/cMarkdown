176f9c272792df39da825970b03b28958587b3b6
Paul Smith <paulsmith@pobox.com>
2011-04-20 12:08:32 -0400

    Implementation optional keyword arguments for the various renderer and MD extension flags

--- src/_markdown.c
+++ src/_markdown.c
@@ -3,28 +3,87 @@
 #include "xhtml.h"
 
 static PyObject *
-markdown_render(PyObject *self, PyObject *args)
+markdown_render(PyObject *self, PyObject *args, PyObject *kwds)
 {
     struct buf *ib, *ob;
     struct mkd_renderer renderer;
     const char *s;
     int size;
+    /* flags - renderer and markdown extensions */
+    int skip_html = 0, skip_style = 0, skip_images = 0, skip_links = 0,
+        smartypants = 0, safelink = 0, toc = 0, hard_wrap = 0, lax_emphasis = 0,
+        tables = 0, fenced_code = 0, autolink = 0, strikethrough = 0,
+        lax_html_blocks = 0;
+    unsigned int render_flags = XHTML_EXPAND_TABS, extensions = 0;
     PyObject *result;
 
-    if (!PyArg_ParseTuple(args, "s#", &s, &size))
+    static char *kwlist[] = {"text", "skip_html", "skip_style", "skip_images",
+                             "skip_links", "smartypants", "safelink",
+                             "toc", "hard_wrap", "lax_emphasis", "tables",
+                             "fenced_code", "autolink", "strikethrough",
+                             "lax_html_blocks", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "s#|iiiiiiiiiiiiii", kwlist, &s, &size,
+            &skip_html, &skip_style, &skip_images, &skip_links, &smartypants,
+            &safelink, &toc, &hard_wrap, &lax_emphasis, &tables,
+            &fenced_code, &autolink, &strikethrough, &lax_html_blocks))
         return NULL;
+
+    /* Set render flags */
+    if (skip_html)
+        render_flags |= XHTML_SKIP_HTML;
+
+    if (skip_style)
+        render_flags |= XHTML_SKIP_STYLE;
+
+    if (skip_images)
+        render_flags |= XHTML_SKIP_IMAGES;
+
+    if (skip_links)
+        render_flags |= XHTML_SKIP_LINKS;
+
+    if (smartypants)
+        render_flags |= XHTML_SMARTYPANTS;
+
+    if (safelink)
+        render_flags |= XHTML_SAFELINK;
+
+    if (toc)
+        render_flags |= XHTML_TOC;
+
+    if (hard_wrap)
+        render_flags |= XHTML_HARD_WRAP;
+
+    /* Set extension flags */
+    if (lax_emphasis)
+        extensions |= MKDEXT_LAX_EMPHASIS;
+
+    if (tables)
+        extensions |= MKDEXT_TABLES;
+
+    if (fenced_code)
+        extensions |= MKDEXT_FENCED_CODE;
+
+    if (autolink)
+        extensions |= MKDEXT_AUTOLINK;
+
+    if (strikethrough)
+        extensions |= MKDEXT_STRIKETHROUGH;
+
+    if (lax_html_blocks)
+        extensions |= MKDEXT_LAX_HTML_BLOCKS;
 
     ib = bufnew(size);
     bufgrow(ib, size+1);
-    ib->data = s;
+    ib->data = (char *)s;
     ib->size = size+1;
 
     ob = bufnew(128);
     bufgrow(ob, size * 1.2);
 
-    ups_xhtml_renderer(&renderer, 0);
+    ups_xhtml_renderer(&renderer, render_flags);
 
-    ups_markdown(ob, ib, &renderer, 0xFF);
+    ups_markdown(ob, ib, &renderer, extensions);
 
     result = Py_BuildValue("s", ob->data);
 
@@ -35,7 +94,7 @@ markdown_render(PyObject *self, PyObject *args)
 }
 
 static PyMethodDef MarkdownMethods[] = {
-    {"markdown", (PyCFunction)markdown_render, METH_VARARGS, PyDoc_STR("Render Markdown to HTML.")},
+    {"markdown", (PyCFunction)markdown_render, METH_VARARGS | METH_KEYWORDS, PyDoc_STR("Render Markdown to HTML.")},
     {NULL, NULL, 0, NULL}
 };
 
