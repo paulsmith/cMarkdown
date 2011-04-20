6bf37f3a144d890e254e4322a7da94047e206c8d
Paul Smith <paulsmith@pobox.com>
2011-04-20 14:23:33 -0400

    Fixed test case generation

--- tests/test.py
+++ tests/test.py
@@ -2,20 +2,24 @@ import glob
 import os
 import re
 import unittest
-import cmarkdown
+import cMarkdown
 
 class TestMarkdown(unittest.TestCase):
     pass
+
+def make_testcase(textfile):
+    def testcase(self):
+        expected_html = open(textfile.replace('.text', '.html')).read().strip()
+        actual_html = cMarkdown.markdown(open(textfile).read()).strip()
+        self.assertEqual(expected_html, actual_html)
+    return testcase
 
 def gen_tests(cls):
     testsdir = os.path.join(os.path.dirname(__file__), 'MarkdownTest_1.0.3', 'Tests')
     for textfile in glob.glob(os.path.join(testsdir, '*')):
         name = os.path.splitext(os.path.basename(textfile))[0]
         fn_name = 'test_' + re.sub('[^a-z0-9_]+', '', re.sub('\s+', '_', name.lower()))
-        def testcase(self):
-            expected_html = open(textfile.replace('.text', '.html')).read()
-            actual_html = cmarkdown.markdown(open(textfile).read())
-            self.assertEqual(actual_html, expected_html)
+        testcase = make_testcase(textfile)
         testcase.__name__ = fn_name
         testcase.__doc__ = 'Markdown test: {0}'.format(name)
         setattr(cls, fn_name, testcase)
