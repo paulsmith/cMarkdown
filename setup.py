b17c388d1fd160d9716fbed831ea4d2439612e28
Paul Smith <paulsmith@pobox.com>
2011-04-21 00:36:30 -0400

    Added README

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
-setup(name='cMarkdown',
-      version='0.1',
-      description='Markdown for Python, accelerated by C.',
-      ext_modules=[Extension(
+setup(
+    name='cMarkdown',
+    version='0.1',
+    description='Markdown for Python, accelerated by C.',
+    author='Paul Smith',
+    author_email='paulsmith@pobox.com',
+    ext_modules=[Extension(
         'cMarkdown', 
         sources=['src/_markdown.c', 'src/markdown.c', 'src/array.c', 'src/buffer.c', 'src/xhtml.c']
-      )]
+    )]
 )
