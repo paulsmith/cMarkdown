358d714a89bfb50c9f2c6ac1cd30e9d4fd82292b
Paul Smith <paulsmith@pobox.com>
2011-04-21 01:17:58 -0400

    Fixed up setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, Extension
+from distutils.core import setup, Extension
 
 setup(
     name='cMarkdown',
@@ -9,5 +9,22 @@ setup(
     ext_modules=[Extension(
         'cMarkdown', 
         sources=['src/_markdown.c', 'src/markdown.c', 'src/array.c', 'src/buffer.c', 'src/xhtml.c']
-    )]
+    )],
+    scripts=['bin/markdown'],
+    url='https://github.com/paulsmith/cMarkdown',
+    download_url='https://github.com/paulsmith/cMarkdown/archives/master',
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Communications :: Email :: Filters',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ]
 )
